797bc3f51d666379626e43c074b1628816a6e822
Hardiing <nathanharding02@gmail.com>
2026-02-09 12:27:53 -0700

    Ch4 done

--- src/textnode.py
+++ src/textnode.py
@@ -38,4 +38,4 @@ def text_node_to_html_node(text_node):
         return LeafNode("a", text_node.text, {"href": text_node.url})
     if text_node.text_type == TextType.IMAGE:
         return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
-    raise Exception("Invalid text type")
+    raise ValueError(f"invalid text type: {text_node.text_type}")
