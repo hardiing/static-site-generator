b300a14d496239f601f63fd0bf500f74317ae636
Hardiing <nathanharding02@gmail.com>
2026-02-11 16:55:21 -0700

    ch 5-1 done

--- src/main.py
+++ src/main.py
@@ -1,7 +1,30 @@
 from textnode import TextNode, TextType
+import os
+import shutil
+import errno
+
+def copy_directory(source, destination):
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+
+    for item in os.listdir(source):
+        from_path = os.path.join(source, item)
+        to_path = os.path.join(destination, item)
+        print(f" * {from_path} copied to {to_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, to_path)
+        else:
+            copy_directory(from_path, to_path)
 
 def main():
-    new_object = TextNode("this is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(new_object)
+    src = "static/"
+    dst = "public/"
+
+    print("deleting public directory...")
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    
+    print("copying static directory to public directory...")
+    copy_directory(src, dst)
 
 main()
